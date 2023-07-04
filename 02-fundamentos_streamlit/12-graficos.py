7195552646cb71816680046727ca9ab9a3918f38
Jorge Harbes <98434001+jharbes@users.noreply.github.com>
2023-07-04 15:29:35 -0300

    Update 12-graficos.py

--- 02-fundamentos_streamlit/12-graficos.py
+++ 02-fundamentos_streamlit/12-graficos.py
@@ -1,11 +1,95 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+import altair as alt
+import plotly.figure_factory as ff
 
 
 # grafico de linha
+st.header('Gráfico SIMPLES Line Chart')
 chart_data = pd.DataFrame(
     np.random.randn(20, 3),
     columns=['a', 'b', 'c'])
 
 st.line_chart(chart_data)
+
+
+
+
+# grafico de area
+st.header('Gráfico SIMPLES de Área (AreaChart)')
+chart_data = pd.DataFrame(
+    np.random.randn(20, 3),
+    columns=['a', 'b', 'c'])
+
+st.area_chart(chart_data)
+
+
+
+
+# grafico de barras
+st.header('Gráfico SIMPLES de Barras (BarChart)')
+chart_data = pd.DataFrame(
+    np.random.randn(20, 3),
+    columns=["a", "b", "c"])
+
+st.bar_chart(chart_data)
+
+
+
+
+# grafico de mapa (scatterplot)
+st.header('Gráfico de mapa (scatterplot)')
+df = pd.DataFrame(
+    np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
+    columns=['lat', 'lon'])
+
+st.map(df)
+
+
+
+
+# graficos com matplotlib
+st.header('Gráfico com MATPLOTLIB')
+arr = np.random.normal(1, 1, size=100)
+fig, ax = plt.subplots()
+ax.hist(arr, bins=20)
+
+st.pyplot(fig)
+
+
+
+
+# graficos com altair
+st.header('Gráficos com ALTAIR')
+chart_data = pd.DataFrame(
+    np.random.randn(20, 3),
+    columns=['a', 'b', 'c'])
+
+c = alt.Chart(chart_data).mark_circle().encode(
+    x='a', y='b', size='c', color='c', tooltip=['a', 'b', 'c'])
+
+st.altair_chart(c, use_container_width=True)
+
+
+
+
+# graficos com plotly
+st.header('Gráficos com PLOTLY')
+# Add histogram data
+x1 = np.random.randn(200) - 2
+x2 = np.random.randn(200)
+x3 = np.random.randn(200) + 2
+
+# Group data together
+hist_data = [x1, x2, x3]
+
+group_labels = ['Group 1', 'Group 2', 'Group 3']
+
+# Create distplot with custom bin_size
+fig = ff.create_distplot(
+        hist_data, group_labels, bin_size=[.1, .25, .5])
+
+# Plot!
+st.plotly_chart(fig, use_container_width=True)
