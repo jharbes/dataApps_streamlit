0c3f65e932173effdb97397d49aca97221dd676b
Jorge Harbes <98434001+jharbes@users.noreply.github.com>
2023-07-04 13:21:45 -0300

    Create 09-st.write_e_st.magic.py

---
+++ 02-fundamentos_streamlit/09-st.write_e_st.magic.py
@@ -0,0 +1,21 @@
+import streamlit as st
+
+st.write('Hello World!')
+st.write('Testando o streamlit!')
+
+
+# O magic funciona apenas digitando o desejado de maneira 'crua' e ele passará para a tela (como se estivesse usando o st.write)
+
+"Minha primeira aplicação"
+'Continuando testando o st.magic'
+
+# string em várias linhas não ficará necessariamente em várias linhas
+"""
+Observe a
+string em várias
+linhas
+"""
+
+string_teste='Jorge Harbes'
+
+string_teste # observe que vai imprimir a string (com magic)
